8f96d42e750e79f8d93c69839668a1e384287ddc
George Royce <georgelroyce@gmail.com>
2015-11-03 20:14:46 -0500

    Comment out segue from StartGameVC and rename segue in Storyboard

---
+++ Frisbee-in-the-D/Player.swift
@@ -0,0 +1,15 @@
+//
+//  Player.swift
+//  Frisbee-in-the-D
+//
+//  Created by GLR on 11/3/15.
+//  Copyright © 2015 George Royce. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    
+    var name = String()
+    
+}
---
+++ Frisbee-in-the-D/SetupGameViewController.swift
@@ -0,0 +1,38 @@
+//
+//  SetupGameViewController.swift
+//  Frisbee-in-the-D
+//
+//  Created by GLR on 11/3/15.
+//  Copyright © 2015 George Royce. All rights reserved.
+//
+
+import UIKit
+
+class SetupGameViewController: UIViewController {
+
+    @IBOutlet weak var player1NameField: UITextField!
+    
+    var player1 = Player()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func startRoundWithName(sender: AnyObject) {
+    }
+
+//    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+//        if segue.identifier == "Start->TabBar" {
+//            let destVC = segue.destinationViewController as! CurrentHoleViewController
+//            // Pass the selected object to the new view controller
+//            player1.name = player1NameField.text!
+//            destVC.userName1.text = player1.name
+//        }
+//    }
+
+
+
+}
