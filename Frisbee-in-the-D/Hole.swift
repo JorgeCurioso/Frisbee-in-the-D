557b555633ccb2649097ab4f169c5b4251a908e8
George Royce <georgelroyce@gmail.com>
2015-11-08 19:14:15 -0500

    Update photos with annotations for where to throw frisbee

--- Frisbee-in-the-D/Hole.swift
+++ Frisbee-in-the-D/Hole.swift
@@ -41,7 +41,7 @@ let hole1 = Hole(name: "1",
                 description: "You must go around the middle post before hitting the one on the right",
                 par: 4,
                 userStrokes: nil,
-                firstPersonTeeImage: (patternImage: UIImage(named: "Hole1Small")),
+                firstPersonTeeImage: (patternImage: UIImage(named: "Hole1")),
                 teeLatitude: 42.329880,
                 teeLongitude: -83.074237)
 
@@ -50,7 +50,7 @@ let hole2 = Hole(name: "2",
                 description: "The alleyway is out of bounds. Be careful as you go for the post!",
                 par: 3,
                 userStrokes: nil,
-                firstPersonTeeImage: (patternImage: UIImage(named: "Hole2Small")),
+                firstPersonTeeImage: (patternImage: UIImage(named: "Hole2")),
                 teeLatitude: 42.329710,
                 teeLongitude: -83.074835)
 
@@ -66,7 +66,7 @@ let hole4 = Hole(name: "4",
                 description: "You must go around the middle post before hitting the one on the right",
                 par: 4,
                 userStrokes: nil,
-                firstPersonTeeImage: (patternImage: UIImage(named: "Hole4Small")),
+                firstPersonTeeImage: (patternImage: UIImage(named: "Hole4")),
                 teeLatitude: 42.329784,
                 teeLongitude: -83.076223)
 
@@ -74,7 +74,7 @@ let hole5 = Hole(name: "5",
                 description: "The trick here is to LAND your frisbee on the tree stump. If it slides off it doesn't count.",
                 par: 3,
                 userStrokes: nil,
-                firstPersonTeeImage: (patternImage: UIImage(named: "Hole5Small")),
+                firstPersonTeeImage: (patternImage: UIImage(named: "Hole5")),
                 teeLatitude: 42.330190,
                 teeLongitude: -83.076476)
 
@@ -82,7 +82,7 @@ let hole6 = Hole(name: "6",
                 description: "This one is equally tricky. Through the 'A', but only from the side you're currently facing (not the far side). Careful not to overshoot; It could cost you!",
                 par: 3,
                 userStrokes: nil,
-                firstPersonTeeImage: (patternImage: UIImage(named: "Hole6Small")),
+                firstPersonTeeImage: (patternImage: UIImage(named: "Hole6")),
                 teeLatitude: 42.330094,
                 teeLongitude: -83.077170)
 
@@ -90,7 +90,7 @@ let hole7 = Hole(name: "7",
                 description: "Starting from at least 5 feet behind, you must go through the 'Roosevelt Park' gate before hitting the tree in view",
                 par: 3,
                 userStrokes: nil,
-                firstPersonTeeImage: (patternImage: UIImage(named: "Hole7Small")),
+                firstPersonTeeImage: (patternImage: UIImage(named: "Hole7")),
                 teeLatitude:  42.330779,
                 teeLongitude: -83.077247)
 
@@ -98,7 +98,7 @@ let hole8 = Hole(name: "8",
                 description: "This hole is a little differet. There is a wall on the far side you must hit (anywhere is fine)",
                 par: 3,
                 userStrokes: nil,
-                firstPersonTeeImage: (patternImage: UIImage(named: "Hole8Small")),
+                firstPersonTeeImage: (patternImage: UIImage(named: "Hole8")),
                 teeLatitude: 42.330910,
                 teeLongitude:  -83.077724)
 
@@ -106,7 +106,7 @@ let hole9 = Hole(name: "9",
                 description: "To complete this hole, you must go through the two central trees before hitting the distant tree on the right",
                 par: 5,
                 userStrokes: nil,
-                firstPersonTeeImage: (patternImage: UIImage(named: "Hole9Small")),
+                firstPersonTeeImage: (patternImage: UIImage(named: "Hole9")),
                 teeLatitude:  42.330878,
                 teeLongitude: -83.078460)
 
